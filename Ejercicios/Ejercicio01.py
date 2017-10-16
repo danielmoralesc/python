83f33ad405ea7b6f7b13874efe8b868e9694a0cb
Daniel Morales Campos <daniel.moralesc@usach.cl>
2017-10-16 00:56:49 -0300

    Update Ejercicio01.py

--- Ejercicios/Ejercicio01.py
+++ Ejercicios/Ejercicio01.py
@@ -1,1 +1,22 @@
 #Ingresar una nota del 1 al 7, si es de 4 hacia arriba mostrar un mensaje de aprobado y en caso contrario mostrar un desaprobado
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 16 00:36:27 2017
+
+@author: danielmorales
+"""
+
+while nota != 0:
+
+    nota = int(input("Ingresa tu nota y te dire si aprobaste o no: "))
+  
+    if nota < 1 or nota > 7:
+        print("Nota ingresada esta fuera del rango")
+        
+    elif nota >= 4 and nota <= 7:
+        print("Has aprobado")
+        
+    else:
+        print("Has reprobado")
